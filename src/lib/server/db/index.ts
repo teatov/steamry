ca08557a6d321b74fd25a5ff40aebaa7da8c694e
teatov <hlebpoimenigleb@gmail.com>
2025-08-30 16:17:16 +1000

    add script to fetch all steam apps

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,14 +1,16 @@
+import dotenv from 'dotenv';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-import { env } from '$env/dynamic/private';
 import * as schema from './schema';
 
+dotenv.config();
+
 const client = postgres({
-  database: env.DB_DATABASE,
-  user: env.DB_USER,
-  password: env.DB_PASSWORD,
-  host: env.DB_HOST,
-  port: Number(env.DB_PORT),
+  database: process.env.DB_DATABASE!,
+  user: process.env.DB_USER!,
+  password: process.env.DB_PASSWORD!,
+  host: process.env.DB_HOST!,
+  port: Number(process.env.DB_PORT!),
 });
 
 export const db = drizzle(client, { schema, casing: 'snake_case' });
---
+++ src/lib/server/scripts/fetch-games.ts
@@ -0,0 +1,60 @@
+import { sql } from 'drizzle-orm';
+import { db } from '../db';
+import * as schema from '../db/schema';
+
+const APP_LIST_URL = 'https://api.steampowered.com/ISteamApps/GetAppList/v2/';
+const CHUNK_SIZE = 20_000;
+
+async function fetchGames() {
+  try {
+    const response = await fetch(APP_LIST_URL);
+    if (!response.ok) {
+      throw new Error(`${response.status} ${response.statusText}`);
+    }
+
+    const result = await response.json();
+    if (!isAppListResponse(result)) {
+      throw new Error('Unexpected response data');
+    }
+
+    const steamApps = result.applist.apps;
+
+    await db.transaction(async (tx) => {
+      console.log('Truncating table...');
+      await tx.execute(sql`TRUNCATE TABLE ${schema.steamApp} RESTART IDENTITY`);
+
+      for (let i = 0; i < steamApps.length; i += CHUNK_SIZE) {
+        const end = Math.min(steamApps.length, i + CHUNK_SIZE);
+        console.log(`Inserting ${i} - ${end}...`);
+        await tx.insert(schema.steamApp).values(steamApps.slice(i, end));
+      }
+    });
+
+    console.log('Finished!');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+type AppListResponse = {
+  applist: {
+    apps: { appid: number; name: string }[];
+  };
+};
+
+function isAppListResponse(data: unknown): data is AppListResponse {
+  return (
+    typeof data === 'object' &&
+    data != null &&
+    'applist' in data &&
+    typeof data.applist === 'object' &&
+    data.applist != null &&
+    'apps' in data.applist &&
+    typeof data.applist.apps === 'object' &&
+    data.applist.apps != null &&
+    Array.isArray(data.applist.apps)
+  );
+}
+
+await fetchGames();
+process.exit(0);
