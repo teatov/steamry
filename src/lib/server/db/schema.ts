fc48c2277419dd5519383ba3145c04f97b129fb6
teatov <hlebpoimenigleb@gmail.com>
2025-08-30 17:04:20 +1000

    add `unique` and `notNull` to `steamApp` columns

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -4,6 +4,9 @@ export const VARCHAR_LENGTH = 255;
 
 export const steamApp = pgTable('steam_app', {
   id: serial().primaryKey(),
-  appid: integer(),
-  name: varchar({ length: VARCHAR_LENGTH }),
+  appid: integer().unique().notNull(),
+  name: varchar({ length: VARCHAR_LENGTH }).notNull(),
 });
+
+export type SteamApp = typeof steamApp.$inferSelect;
+export type NewSteamApp = typeof steamApp.$inferInsert;
