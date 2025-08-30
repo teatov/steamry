038fcad63d7ca7864f6d2228f8211dd62ef3d957
teatov <hlebpoimenigleb@gmail.com>
2025-08-30 15:25:46 +1000

    replace database url with object

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'drizzle-kit';
 
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-
 export default defineConfig({
   schema: './src/lib/server/db/schema.ts',
   dialect: 'postgresql',
-  dbCredentials: { url: process.env.DATABASE_URL },
+  dbCredentials: {
+    database: process.env.DB_DATABASE!,
+    user: process.env.DB_USER!,
+    password: process.env.DB_PASSWORD!,
+    host: process.env.DB_HOST!,
+    port: Number(process.env.DB_PORT!),
+  },
   verbose: true,
   strict: true,
 });
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,8 +3,12 @@ import postgres from 'postgres';
 import { env } from '$env/dynamic/private';
 import * as schema from './schema';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-
-const client = postgres(env.DATABASE_URL);
+const client = postgres({
+  database: env.DB_DATABASE,
+  user: env.DB_USER,
+  password: env.DB_PASSWORD,
+  host: env.DB_HOST,
+  port: Number(env.DB_PORT),
+});
 
 export const db = drizzle(client, { schema });
