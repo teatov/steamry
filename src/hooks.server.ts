acdaaf16e77fac7f02b21c46bc3d940b0434cb17
teatov <hlebpoimenigleb@gmail.com>
2025-08-31 11:23:33 +1000

    add cron job for fetching apps weekly

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,14 +1,23 @@
 import type { ServerInit } from '@sveltejs/kit';
 import { CronJob } from 'cron';
 import generateDailies from '$lib/server/daily/generate-dailies';
+import fetchApps from '$lib/server/steam/fetch-apps';
 
 export const init: ServerInit = async () => {
   if (import.meta.env.PROD) {
+    await fetchApps();
     await generateDailies();
 
     CronJob.from({
       cronTime: '00 12 00 * * *',
       onTick: generateDailies,
+      start: true,
+      utcOffset: 0,
+    });
+
+    CronJob.from({
+      cronTime: '00 06 00 * * 1',
+      onTick: fetchApps,
       start: true,
       utcOffset: 0,
     });
