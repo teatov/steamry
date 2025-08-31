420df3c17e62a88c711383ebb40acd4f8fd72fcf
teatov <hlebpoimenigleb@gmail.com>
2025-08-31 16:46:09 +1000

    fix deployment

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,3 @@
-import type { ServerInit } from '@sveltejs/kit';
 import { CronJob } from 'cron';
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import { building } from '$app/environment';
@@ -6,27 +5,25 @@ import generateDailies from '$lib/server/daily/generate-dailies';
 import { db } from '$lib/server/db';
 import fetchApps from '$lib/server/steam/fetch-apps';
 
-export const init: ServerInit = async () => {
-  if (import.meta.env.PROD && !building) {
-    console.log('Migrating...');
-    await migrate(db, { migrationsFolder: './drizzle' });
-    console.log('Migrations done!');
+if (import.meta.env.PROD && !building) {
+  console.log('Migrating...');
+  await migrate(db, { migrationsFolder: './drizzle' });
+  console.log('Migrations done!');
 
-    await fetchApps();
-    await generateDailies();
+  await fetchApps();
+  await generateDailies();
 
-    CronJob.from({
-      cronTime: '00 12 00 * * *',
-      onTick: generateDailies,
-      start: true,
-      utcOffset: 0,
-    });
+  CronJob.from({
+    cronTime: '00 12 00 * * *',
+    onTick: generateDailies,
+    start: true,
+    utcOffset: 0,
+  });
 
-    CronJob.from({
-      cronTime: '00 06 00 * * 1',
-      onTick: fetchApps,
-      start: true,
-      utcOffset: 0,
-    });
-  }
-};
+  CronJob.from({
+    cronTime: '00 06 00 * * 1',
+    onTick: fetchApps,
+    start: true,
+    utcOffset: 0,
+  });
+}
