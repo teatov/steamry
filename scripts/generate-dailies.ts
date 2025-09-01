80a54a50ef09ebb8aa21c36136271392d7465c76
teatov <hlebpoimenigleb@gmail.com>
2025-09-02 00:13:47 +1000

    make date local in `generate-dailies` script

--- scripts/generate-dailies.ts
+++ scripts/generate-dailies.ts
@@ -1,7 +1,7 @@
-import { getTodayDate } from '../src/lib';
+import { getTimezoneDate } from '../src/lib';
 import makeNewDaily from '../src/lib/server/daily/make-new-daily';
 
-const today = getTodayDate();
+const today = getTimezoneDate(new Date().getTimezoneOffset());
 await makeNewDaily(today);
 
 process.exit(0);
