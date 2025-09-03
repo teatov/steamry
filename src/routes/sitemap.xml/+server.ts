5957d4652f010e85cbe4bd35615c15ef9fe4ddcc
teatov <hlebpoimenigleb@gmail.com>
2025-09-04 00:23:03 +1000

    add sitemap

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,10 @@
+import type { RequestHandler } from '@sveltejs/kit';
+import * as sitemap from 'super-sitemap';
+import { env } from '$env/dynamic/private';
+
+export const GET: RequestHandler = async () => {
+  return await sitemap.response({
+    origin: env.ORIGIN,
+    excludeRoutePatterns: ['^/play*'],
+  });
+};
