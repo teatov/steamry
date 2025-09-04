990ae9094620a4d3bcef261dbbbdc2c1aec59180
teatov <hlebpoimenigleb@gmail.com>
2025-09-04 23:49:41 +1000

    add PWA features and prerendering

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -2,6 +2,8 @@ import type { RequestHandler } from '@sveltejs/kit';
 import * as sitemap from 'super-sitemap';
 import { env } from '$env/dynamic/private';
 
+export const prerender = true;
+
 export const GET: RequestHandler = async () => {
   return await sitemap.response({
     origin: env.ORIGIN,
