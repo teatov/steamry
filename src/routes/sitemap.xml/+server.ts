fe3bba649ca5bf9731f523c442d51d1f7674efcd
teatov <hlebpoimenigleb@gmail.com>
2025-09-07 05:47:56 +1000

    enable sitemap prerendering

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,6 +1,8 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import * as sitemap from 'super-sitemap';
 
+export const prerender = true;
+
 export const GET: RequestHandler = async () => {
   return await sitemap.response({
     origin: process.env.ORIGIN!,
